642c800338b2e4fbaeee1f71c3f5e129f9403c12
shenming <569818710@qq.com>
2017-11-28 10:47:57 +0800

    create demo

---
+++ Demo/SXMArmyDemo/SXMArmyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SXMArmyDemo
+//
+//  Created by syn on 2017/11/28.
+//  Copyright © 2017年 sxm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo/SXMArmyDemo/SXMArmyDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SXMArmyDemo
+//
+//  Created by syn on 2017/11/28.
+//  Copyright © 2017年 sxm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
